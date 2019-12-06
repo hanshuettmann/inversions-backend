f34cbbf37334b788cdb2e81398f9458fd6f9137e
leomoyano <leomoyano7@gmail.com>
2019-12-05 21:37:44 -0300

    first

--- src/app.js
+++ src/app.js
@@ -7,12 +7,15 @@ import indexRouter from './routes/index';
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
-app.use('/', indexRouter);
+app.use('/inversions', indexRouter);
 
 export default app;
